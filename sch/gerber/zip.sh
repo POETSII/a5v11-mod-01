ac3297d0fcc601d329f128eb3a1ac587c6bac5d9
Theo Markettos <git+atm26@cl.cam.ac.uk>
2018-03-06 15:16:01 +0000

    Pull back flexi connector from edge of board. Respin gerbers

--- sch/gerber/zip.sh
+++ sch/gerber/zip.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-DATE=`date +%Y%M%d-%H%m`
+DATE=`date +%Y%m%d-%H%M`
+DIR=proto1panel
 
-zip -rv9 tinyusbhub-$DATE.zip proto1panel/ -x proto1panel/combined.drl
+# merge excellon files from Kicad and GerberPanelizer
+#GerberCombiner $DIR/merged-drills.txt $DIR/combined.drl $DIR/combined.txt
+#mv $DIR/merged-drills.txt $DIR/merged-drills.drl
+zip -rv9 tinyusbhub-$DATE.zip $DIR/ -x $DIR/combined.drl -x $DIR/combined.svg -x $DIR/combined.txt 
 
